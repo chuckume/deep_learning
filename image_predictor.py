9bc018fdaf01c30c76a5b3d1b7e4bd09cb022945
chuckume <chuckchinaqc@gmail.com>
2019-09-25 17:16:17 -0400

    Added prediction confidence

--- image_predictor.py
+++ image_predictor.py
@@ -2,12 +2,12 @@ from fastai.vision import *
 from pathlib import Path
 
 # Make sure that path contains the file 'export.pkl' from before.
-path=Path('path_to_export.pkl_file')
-
-learn = load_learner(path)
+#path=Path('path_to_export.pkl_filelearn = load_learner(path)
 
 defaults.device = torch.device('cpu')
 
 img = open_image(path/'AIGLE.png')
 pred_class,pred_idx,outputs = learn.predict(img)
-pred_class
+print( {"prediction": pred_class.__dict__['obj'],
+        "confidence": outputs.numpy()[pred_class.__dict__['data'].item()]*100})
+
